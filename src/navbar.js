cf7651bdf80b9acdd383ba7642103c02a692cabc
Seth Watson <seth.watson1@gmail.com>
2017-07-28 16:03:13 -0600

    installed react bootstap added dropdown button

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar">
-                <span>Some Squanch</span>
-                <span><a href="#">Some More Squanch</a></span>
-                <span><a href="#">Even More Squanch</a></span>
-                <span><a href="#">Squanch Something</a></span>
+                <span><a href="#">Home</a></span>
+                <span><a href="#">Contact</a></span>
+                <span><a href="#">Blog</a></span>
+                <span>
+                    <DropdownButton>
+                        <MenuItem eventKey="1">Code </MenuItem>
+                        <MenuItem eventKey="2">Expo </MenuItem>
+                    </DropdownButton>
+                </span>
             </div>
         )
     }
